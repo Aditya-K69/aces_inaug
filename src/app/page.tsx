a36fb42fe088dd41ffde914e3c5bcd40e7d5a39c
Aditya-K69 <adityakelkar1209@gmail.com>
2025-09-10 19:17:28 +0530

    tried adding timer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [timer, setTimer] = useState(10);
+
   useEffect(() => {
     const interval = setInterval(async () => {
       const res = await fetch("/api/trigger", {
@@ -11,18 +13,27 @@ export default function Home() {
       });
 
       const data = await res.json();
-      console.log(data)
+      console.log(data);
+
       if (data.trigger === true) {
-        window.location.href = "https://aces-website-2025-26.vercel.app/";
+        setTimer((prev) => (prev > 0 ? prev - 1 : 0));
+        if (timer === 1) {
+          window.location.href = "https://aces-website-2025-26.vercel.app/";
+        }
       }
-    }, 500);
+    }, 1000); // poll every second
 
     return () => clearInterval(interval);
-  }, []);
+  }, [timer]);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-2xl">
-      ACES INAUGURATION WEBSITE
+    <div>
+      <div className="flex flex-col items-center justify-center h-screen text-2xl">
+        ACES INAUGURATION WEBSITE Time to Reveal : {timer}
+      </div>
+      <div className="text-white">
+        
+      </div>
     </div>
   );
 }
